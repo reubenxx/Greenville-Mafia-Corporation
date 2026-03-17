64b670cd84f6dc2dfa455ddade9b329846782a73
reubenxx <95737472+reubenxx@users.noreply.github.com>
2026-03-17 21:39:44 +1100

    Update main.py

--- main.py
+++ main.py
@@ -59,12 +59,12 @@ async def on_member_join(member):
             "We are honored to have you here with us! Before you venture off into **GVMC**, "
             "please **[verify](https://discord.com/channels/1441901639739904125/1471452917163884738)** "
             "to gain full access to our server.\n\n"
-            "> <a:pulsating_heart:1478774678645637160> We host daily Convoys, Events, Occasional Giveaways "
+            "> <a:pulsatingheart:1478774678645637160> We host daily Convoys, Events, Occasional Giveaways "
             "and other fun surprises! We look forward to seeing you participate in the full life of "
             "__**Greenville Mafia Corporation**__. If you require any form of assistance, please do not "
             "hesitate to contact our lovely Staff Team "
             "**[here](https://discord.com/channels/1441901639739904125/1443980437184577556)**. "
-            "<a:pulsating_heart:1478774678645637160>"
+            "<a:pulsatingheart:1478774678645637160>"
         )
     )
 
