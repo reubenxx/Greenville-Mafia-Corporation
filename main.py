faab37fa4ced07f88fb8a69a112981b4eccf9d89
reubenxx <95737472+reubenxx@users.noreply.github.com>
2026-03-25 18:20:06 +1100

    Update main.py

--- main.py
+++ main.py
@@ -374,6 +374,8 @@ async def loa(interaction: discord.Interaction, reason: str, start_date: str, en
             "> Your LOA has been submitted. Our Management team will review it shortly.\n\n"
             "> Please look out for a **Direct Message** confirming your submission and another when we have approved or denied your submission.\n\n"
             "> If there are any issues, contact management."
+
+            "> If you do not get a DM response within 1 day, please create a ticket."
         ),
         color=0x87CEFA
     )
