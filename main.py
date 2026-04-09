db03a2bd12f4434b05eab14281346996c8f24844
reubenxx <95737472+reubenxx@users.noreply.github.com>
2026-04-09 21:52:01 +1000

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="Watching over 'Greenville Mafia Corporation'"
+            name="Watching over 'Greenville Roleplay Global'"
         )
     )
     bot.add_view(LOAView(0, 0, 0))
